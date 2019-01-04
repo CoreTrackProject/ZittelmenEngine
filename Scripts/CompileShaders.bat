4a69a1efbf7d1fa0145cb3c5da54c12e1735daf6
Phillip <rphillip.thomas@gmail.com>
2019-01-04 23:44:26 +0100

    Create CompileShaders.bat

---
+++ Scripts/CompileShaders.bat
@@ -0,0 +1,12 @@
+@echo off
+
+setlocal
+
+SET PATH=%PATH%;"..\..\vcpkg\installed\x64-windows\tools\"
+
+cd ..\Source\Vulkan\Shaders\
+
+glslangvalidator -V color.vert -o color_vert.spv
+glslangvalidator -V color.frag -o color_frag.spv
+
+pause
