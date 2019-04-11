fd9fd3fab1ab22f3a6840eab82e96b1a3cd470c9
Phillip Thomas <example@example.com>
2019-04-11 13:41:53 +0200

    WIP Implement triangle example

--- Examples/SimpleTriangle/Main.cpp
+++ Examples/SimpleTriangle/Main.cpp
@@ -4,11 +4,33 @@
 #include <iostream>
 #include <string>
 
+#include <ZittelmenEngine.h>
+
+std::unique_ptr<ZittelmenEngine> engine = std::make_unique<ZittelmenEngine>();
+
+void timer_tick()
+{
+	engine->renderFrame();
+}
+
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
 
+	
+
     QWindow w;
+	w.setWidth(1280);
+	w.setHeight(720);
+
+	engine->setTargetRenderSurface(&w);
+	engine->initialize();
+
+	QTimer renderTimer(&w);
+	//QObject::connect(renderTimer, &QTimer::timeout, w, &timer_tick);
+
+	//renderTimer.start(0);
+
     w.show();
 
     return a.exec();
